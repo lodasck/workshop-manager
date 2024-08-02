592049f2014c5320c5032d1f3bc5c1876fe37524
lodasck <raven.dev.m@gmail.com>
2024-08-02 12:48:04 +0200

    create form contact

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,44 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ContactFormType;
+use App\Service\ContactService;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    private ContactService $contactService;
+
+    public function __construct(ContactService $contactService)
     {
-        return new response ();
+        $this->contactService = $contactService;
+    }
+
+    #[Route('/', name: 'app_home')]
+    public function sendEmail(Request $request): Response
+    {
+        $form = $this->createForm(ContactFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $this->contactService->sendEmail(
+                $data['Email'], 
+                $data['Objet'], 
+                $data['Contenu']
+            );
+            
+            $this->addFlash('success', 'Email envoyé avec succès !');
+            return $this->redirectToRoute('app_home');
+
+        }
+
+        return $this->render('home/home.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
